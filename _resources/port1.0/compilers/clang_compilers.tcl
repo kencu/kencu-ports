7de2dbbc3cce648d36590c957b33b9eeff122a6b
Ken Cunningham <kencu@macports.org>
2021-10-27 09:55:25 -0700

    compilers: support clang-13, default to clang-13

--- _resources/port1.0/compilers/clang_compilers.tcl
+++ _resources/port1.0/compilers/clang_compilers.tcl
@@ -4,22 +4,27 @@
 # clang_dependency PortGroup, and add it to any new dependencies of the
 # new version.
 
-# clang 11  and older build on 10.6+  (darwin 10)
-# clang 7.0 and older build on 10.5+  (darwin 9)
-# clang 3.4 and older build on 10.4+  (darwin 8)
+# clang 13  and older build on 10.6+  (darwin 10)
+# clang 9.0 and older build on 10.5+  (darwin 9)
+# clang 7.0 and older build on 10.4+  (darwin 8)
 # Clang 11 and newer only on Apple Silicon
 # Clang 9.0 and newer only on 11+ (Darwin 20)
 
 global os.major os.arch
 
-#if { $${os.major} >= 10 } {
-    # TODO: evaluate compatibility of clang-{10,11} as defaults instead of 9.0
-    #lappend compilers macports-clang-11
-#}
 
-if { ${os.arch} ne "arm" && ${os.major} >= 10 } {
-    # TODO: evaluate compatibility of clang-{10,11} as defaults instead of 9.0
-    #lappend compilers macports-clang-10
+if { ${os.major} >= 10 } {
+    lappend compilers macports-clang-13
+    lappend compilers macports-clang-12
+}
+if { ${os.major} >= 10 } {
+    lappend compilers macports-clang-11 
+    if { ${os.arch} ne "arm" } {
+        lappend compilers macports-clang-10
+    }
+}
+
+if { ${os.arch} ne "arm" && ${os.major} >= 9 } {
     lappend compilers macports-clang-9.0
     if { ${os.major} < 20 } {
         lappend compilers macports-clang-8.0
@@ -30,17 +35,6 @@ if { ${os.arch} ne "arm" && ${os.major} >= 9 && ${os.major} < 20 } {
     lappend compilers macports-clang-7.0 \
                       macports-clang-6.0 \
                       macports-clang-5.0
-}
-
-# Add 10+ as defaults *after* 9.0 - 5.0
-if { ${os.major} >= 11 } {
-    lappend compilers macports-clang-12
-}
-if { ${os.major} >= 10 } {
-    lappend compilers macports-clang-11 
-    if { ${os.arch} ne "arm" } {
-        lappend compilers macports-clang-10
-    }
 }
 
 if { ${os.major} < 16 } {
